065411115069b6ff41c40df61a5bb39dad726fc9
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2026-04-17 10:40:51 +0300

    add_web_socket

--- server/main.go
+++ server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,42 +10,93 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type Response struct {
-	Count int `json:"count"`
-}
+// type Response struct {
+// 	Count int `json:"count"`
+// }
 
 var (
-	counter int
+	//counter int
+	board = [9]string{"","","","","","","","",""}
+	currentSymbol = "X" 
 	mu sync.Mutex // Потрібно для безпечної роботи з данними з різних потоків
 
-	// clients = make(map[*websocket.Conn]bool)
-	// broadcast = make(chan int) // Chanal to sending new value
-	// upgrader = websocket.Upgrader{
-	// 	CheckOrigin: func(r *http.Request) bool {return true}, // Дозволяємо всі підключення
-	// }
+	clients = make(map[*websocket.Conn]bool)
+	broadcast = make(chan [9]string) // Chanal to sending new value
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {return true}, // Дозволяємо всі підключення
+	}
 )
 
-func incrementHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Тільки POST запити", http.StatusMethodNotAllowed)
-		return
-	}
+func main() {
+	//http.HandleFunc("/increment", incrementHandler)
+	http.HandleFunc("/ws", handleConnections)
 
-	// Захищаємо змінну від одночасного запису
-	mu.Lock()
-	counter++
-	current := counter
-	mu.Unlock()
+	// Окремий потік для розсилки оновлень
+	go handleMessages()
 
-	// Відправляємо відповідь у формі JSON
-	w.Header().Set("Counter-Type", "application/json")
-	json.NewEncoder(w).Encode(Response{Count: current})
-	fmt.Printf("Лог: Лічильник збільшився до %d\n", current)
+	fmt.Println("WebSocket сервер запущено на :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func main() {
-	http.HandleFunc("/increment", incrementHandler)
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	} 
+	defer ws.Close()
 
-	fmt.Println("http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	clients[ws] = true
+
+	ws.WriteJSON(map[string]any{"board": board})
+
+	for {
+		// Чекаємо на повідомлення від клієнта (наприклад, сигнал про натискання)
+		var msg map[string]int
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			break
+		}
+		fmt.Printf("handleConnections() msg is: %v\n", msg)
+
+		idx := msg["index"]
+		// Логіка 
+		mu.Lock()
+		if board[idx] == "" {
+			board[idx] = currentSymbol
+			if currentSymbol == "X" {
+				currentSymbol = "O"
+			} else {
+				currentSymbol = "X"
+			}
+		}
+		mu.Unlock()
+
+		// Відправляємо нове значення в канал для розсилки всім
+		broadcastBoard()
+	}
+}
+
+func broadcastBoard() {
+	mu.Lock()
+	currentBoard := board
+	mu.Unlock()
+
+	broadcast <- currentBoard
+}
+
+func handleMessages() {
+	for {
+		updatedBoard := <-broadcast
+		msg := map[string]any{"board": updatedBoard}
+
+		// Send to all connected clients
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("Помилка відправки клієнту: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
